465d932475ad09371517d8c7ba329fc2d724d90c
BlakeWewer <bewewer@cougar.cs.ksu.edu>
2020-02-25 14:55:27 -0600

    Work from 2/25/20

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -31,6 +31,9 @@ process_execute (const char *file_name)
   char *fn_copy;
   tid_t tid;
 
+  char *split_file_name;
+  file_name = __strtok_r(file_name, " ", &split_file_name);
+
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
